1af853600f5a1f9dca924aedc90ac982e471549c
cgeek <cgeek.share@gmail.com>
2011-12-05 16:01:58 +0800

    first commit

---
+++ application/views/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>后台管理</title>
+    <link href="/static/css/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <div class="header-sub">
+            <div class="logo">
+                <a title="" href="">嘟嘟鸟</a>
+            </div>
+            <ul class="top-nav">
+                <li><a href="/">后台首页</a></li>
+                <li><a href="/client">手机客户端管理</a></li>
+                <li><a href="/place">目的地管理</a></li>
+            </ul>
+            <ul class="top-menu">
+                <li><a href="/user/cgeek">cgeek</a></li>
+                <li><a href="/logout">登出</a></li>
+            </ul>
+        </div>
+    </div>
---
+++ application/views/home.php
@@ -0,0 +1,36 @@
+<?php $this->load->view('header');?>
+    <div class="wrap">
+        <div class="content">
+            <div class="sidebar">
+                <div class="sidebar-nav">
+                    <a class="tit on-prev" href="javascript:;">客户端版本管理</a>
+                    <a class="on" href="#">客户端升级管理</a>
+                    <a class="on-next" href="#">iphone push管理</a>
+                    <a href="#">客户端查看</a>
+                </div>
+                
+            </div>
+            <div class="main">
+                <div class="submain">
+                    <h1 class="sitetit">
+                        tit
+                    </h1>
+                    <h2 class="sitetip">
+                        tip
+                    </h2>
+
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <div class="footer">
+        <div class="footer-sub">
+
+            版权所有 &copy; 2011 
+        </div>
+    </div>
+</body>
+</html>
+    
